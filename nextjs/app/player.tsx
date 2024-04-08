8721c4d961c68891b87536f19cd150b30821e672
Mikko Kotamies <mikko.kotamies@xa.epicgames.com>
2024-04-08 10:21:55 +0300

    next: render 20000 images

--- nextjs/app/player.tsx
+++ nextjs/app/player.tsx
@@ -1,5 +1,6 @@
 "use server";
 import { ReactElement } from "react";
+import Image from "next/image";
 
 export type Player = {
   name: string;
@@ -9,9 +10,14 @@ export type Player = {
 const Player = async ({ name, score }: Player): Promise<ReactElement> => {
   return (
     <div>
-      {/* <Image src="/player.png" width={50} height={50} /> */}
       <span>{name}</span>
       <span>{score}</span>
+      <Image
+        alt="player image"
+        src="https://images.unsplash.com/photo-1711211095357-076c9784660d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width={50}
+        height={50}
+      />
     </div>
   );
 };
