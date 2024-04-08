f2c81cf8a21c38b3afae4817e96ef2bd3d7f3d87
Mikko Kotamies <mikko.kotamies@xa.epicgames.com>
2024-04-08 10:53:01 +0300

    next: use htlm img element

--- nextjs/app/player.tsx
+++ nextjs/app/player.tsx
@@ -1,6 +1,5 @@
 "use server";
 import { ReactElement } from "react";
-import Image from "next/image";
 
 export type Player = {
   name: string;
@@ -12,7 +11,7 @@ const Player = async ({ name, score }: Player): Promise<ReactElement> => {
     <div>
       <span>{name}</span>
       <span>{score}</span>
-      <Image
+      <img
         alt="player image"
         src="https://images.unsplash.com/photo-1711211095357-076c9784660d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         width={50}
