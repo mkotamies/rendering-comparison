1ee549296c05e78e5fc674b605d49b7f76bac314
Mikko Kotamies <mikko.kotamies@xa.epicgames.com>
2024-04-23 10:07:54 +0300

    rakkas: render images

--- rakkas/src/routes/ssr.page.tsx
+++ rakkas/src/routes/ssr.page.tsx
@@ -13,6 +13,12 @@ const SSR: Page = function SSR() {
           <div key={player.name}>
             <span>{player.name}</span>
             <span>{player.score}</span>
+            <img
+              alt="player image"
+              src="https://images.unsplash.com/photo-1711211095357-076c9784660d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              width={50}
+              height={50}
+            />
           </div>
         ))}
     </main>
